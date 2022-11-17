cf2d10f4ebd9bd0062f71b5c19ca07fa2fb7e280
Wudext <jalchinn@yandex.ru>
2022-11-17 14:41:14 +0300

    Update main_fastapi.py

--- main_fastapi.py
+++ main_fastapi.py
@@ -16,7 +16,7 @@ def get_db():
         db.close()
 
 
-@app.post("/buttons/", response_model=CRUD.Button)
+@app.post("/button/", response_model=CRUD.Button)
 def create_button(button: CRUD.ButtonCreate, db: Session = Depends(get_db)):
     db_button = methods.get_button(db, button_id=button.id)
     if db_button:
@@ -24,12 +24,12 @@ def create_button(button: CRUD.ButtonCreate, db: Session = Depends(get_db)):
     return methods.create_button(db=db, button=button)
 
 
-@app.get("/buttons/", response_model=list[CRUD.Button])
+@app.get("/button/", response_model=list[CRUD.Button])
 def get_buttons(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return methods.get_buttons(db, skip=skip, limit=limit)
 
 
-@app.get("/buttons/{button_id}", response_model=CRUD.Button)
+@app.get("/button/{button_id}", response_model=CRUD.Button)
 def get_button(button_id: int, db: Session = Depends(get_db)):
     db_button = methods.get_button(db, button_id)
     if db_button is None:
@@ -37,7 +37,7 @@ def get_button(button_id: int, db: Session = Depends(get_db)):
     return db_button
 
 
-@app.delete("/buttons/")
+@app.delete("/button/")
 def remove_button(button_id: int, db: Session = Depends(get_db)):
     db_button = methods.get_button(db, button_id)
     if db_button is None:
@@ -45,7 +45,7 @@ def remove_button(button_id: int, db: Session = Depends(get_db)):
     return methods.delete_button(db=db, button_id=button_id)
 
 
-@app.patch("/buttons/")
+@app.patch("/button/")
 def update_button(button: CRUD.ButtonCreate, db: Session = Depends(get_db)):
     db_old_button = get_button(button_id=button.id, db=db)
     if db_old_button is None:
@@ -53,7 +53,7 @@ def update_button(button: CRUD.ButtonCreate, db: Session = Depends(get_db)):
     return methods.update_button(db=db, button_id=button.id, button=button)
 
 
-@app.post("/categories/", response_model=CRUD.Category)
+@app.post("/category/", response_model=CRUD.Category)
 def create_category(category: CRUD.CategoryCreate, db: Session = Depends(get_db)):
     db_category = methods.get_category(category_id=category.id, db=db)
     if db_category:
@@ -61,12 +61,12 @@ def create_category(category: CRUD.CategoryCreate, db: Session = Depends(get_db)
     return methods.create_category(db=db, category=category)
 
 
-@app.get("/categories/", response_model=list[CRUD.Category])
+@app.get("/category/", response_model=list[CRUD.Category])
 def get_categories(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return methods.get_categories(db, skip=skip, limit=limit)
 
 
-@app.get("/categories/{category_id}", response_model=CRUD.Category)
+@app.get("/category/{category_id}", response_model=CRUD.Category)
 def get_category(category_id: int, db: Session = Depends(get_db)):
     db_category = methods.get_category(category_id=category_id, db=db)
     if db_category is None:
@@ -74,7 +74,7 @@ def get_category(category_id: int, db: Session = Depends(get_db)):
     return db_category
 
 
-@app.delete("/categories/")
+@app.delete("/category/")
 def remove_category(category_id: int, db: Session = Depends(get_db)):
     db_category = methods.get_category(db, category_id=category_id)
     if db_category is None:
@@ -82,7 +82,7 @@ def remove_category(category_id: int, db: Session = Depends(get_db)):
     return methods.delete_category(db=db, category_id=category_id)
 
 
-@app.patch("/categories/")
+@app.patch("/category/")
 def update_category(category: CRUD.CategoryCreate, db: Session = Depends(get_db)):
     db_old_category = methods.get_category(db=db, category_id=category.id)
     if db_old_category is None:
