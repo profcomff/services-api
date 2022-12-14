9d71ec121eaef4a993ccb8c4110e687c8d69e833
MikeP <parfenov.ma21@physics.msu.ru>
2022-12-14 17:25:45 +0300

    typo fix

--- services_backend/routes/base.py
+++ services_backend/routes/base.py
@@ -25,5 +25,5 @@ app.add_middleware(
     allow_headers=settings.CORS_ALLOW_HEADERS,
 )
 
-app.include_router(button, prefix='/button', tags=["category"])
-app.include_router(category, prefix='/category', tags=["button"])
+app.include_router(button, prefix='/button', tags=["Button"])
+app.include_router(category, prefix='/category', tags=["Category"])
