309a6fed01dff79088d1e7285c4116a9159b9bdf
MikeP <parfenov.ma21@physics.msu.ru>
2022-12-14 17:13:57 +0300

    tests initial with base conftest

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
+from services_backend.routes.base import app
+from services_backend.settings import get_settings
+from services_backend.models.base import Base
+
+
+@pytest.fixture(scope='session')
+def client():
+    client = TestClient(app)
+    return client
+
+
+@pytest.fixture(scope='session')
+def dbsession() -> Session:
+    settings = get_settings()
+    engine = create_engine(settings.DB_DSN)
+    TestingSessionLocal = sessionmaker(autocommit=True, autoflush=False, bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield TestingSessionLocal()
+    Base.metadata.drop_all(bind=engine)
