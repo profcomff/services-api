fb3d11df04abf4ed9c94f2e2564c7d1f0a391e00
Wudext <jalchinn@yandex.ru>
2023-02-02 19:31:31 +0300

    Fixing

--- services_backend/models/database.py
+++ services_backend/models/database.py
@@ -1,19 +1,15 @@
-<<<<<<< Updated upstream
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-=======
 from __future__ import annotations
 from sqlalchemy import Integer, String, ForeignKey, PickleType
 from sqlalchemy.orm import relationship, Mapped, mapped_column
->>>>>>> Stashed changes
 from .base import Base
 
 
 class Category(Base):
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    type = Column(String)
-    buttons = relationship("Button", back_populates="category", foreign_keys="Button.category_id")
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    order: Mapped[int] = mapped_column(Integer)
+    name: Mapped[str] = mapped_column(String)
+    type: Mapped[str] = mapped_column(String)
+    buttons: Mapped[Button] = relationship("Button", back_populates="category", foreign_keys="Button.category_id")
 
 
 class Button(Base):
